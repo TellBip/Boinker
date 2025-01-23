36bf7bd75735806fb35bf390f10e45f3315d489e
telbip <telbipmy@proton.me>
2025-01-23 12:27:20 +0200

    upd

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -972,7 +972,7 @@ class Tapper:
                 live_op , hash = await self.get_liveOpId(http_client)
                 
                 self.info(f"Hash - {hash}")
-                if hash != '671293748':
+                if hash != '1461595111':
                     self.warning(f"Please STOP - NEED UPDATE")
                     break
                 await asyncio.sleep(delay=2)
